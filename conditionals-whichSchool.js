3061c2aa28963ad8e5b178c6b7fb3c52ab7a21fe
Samson Poon <samsonkfpoon@gmail.com>
2022-10-31 20:15:08 +0000

    whichSchool exercise

---
+++ conditionals-whichSchool.js
@@ -0,0 +1,13 @@
+const whichSchool = function (age) {
+  if (age < 13) {
+    return "Elementary School";
+  }
+  else if (age > 12 && age < 19) {
+    return "Secondary School"
+  }
+  else {
+    return "Lighthouse Labs"
+  }
+    
+}
+console.log(whichSchool(52));
